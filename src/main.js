a90b00cb3aaaff5904982f71ef2093da16a004a5
Jose Luis Simione <juniorsimione@gmail.com>
2024-01-18 18:25:17 -0300

    função copiar

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { nanoid } from 'nanoid'
-import "./style.css";
+import './style.css';
+import copy from 'clipboard-copy';
 
 
 const passwordBtnEl = document.querySelector('button');
@@ -9,3 +10,10 @@ passwordBtnEl.addEventListener('click', () => {
   const randomPassword = nanoid();
   displayPasswordEl.innerHTML = randomPassword;
 });
+
+displayPasswordEl.addEventListener('click', (event) => {
+    copy(event.target.innerHTML);
+    alert('Senha copiada!');
+  });
+
+  
